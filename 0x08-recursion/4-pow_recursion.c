cb49d1734a1d333b1b79c9392f40570e53a602e9
Kamblossi <newton.ochieng97@gmail.com>
2024-01-12 03:19:06 +0300

    4

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _pow_recursion - function that returns the value of x raised to the power of y
+ * _pow_recursion - value of x raised to the power of y
  * @x: value
  * @y: exponent
  *
