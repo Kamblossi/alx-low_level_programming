2161e4690b704b20b0a57c70347f8a2d9ba8b26d
Kamblossi <newton.ochieng97@gmail.com>
2024-01-17 15:54:48 -0800

    let us go

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,51 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "main.h"
 
 /**
- *_change - Count the minimum number of coins to make change
- *for an amount of money.
- *@num: Amount.
- *@mod: Array of coins.
- *@size: Size of array.
- *Return: The minimum number of coins.
- */
-
-int _change(int num, int *mod, int size)
-{
-		if (num < *mod)
-					return (0);
-			return ((num / *(mod + size - 1)) +
-						       _change((num % *(mod + size - 1)), mod, size - 1));
-}
-
-/**
- *main - Print the change for an amount of money.
- *@argc: Number of arguments.
- *@argv: Array of arguments.
- *Return: 0 is succes.
+ * main - prints the minimum number of coins to
+ * make change for an amount of money
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: 0 (Success), 1 (Error)
  */
 int main(int argc, char *argv[])
 {
-		int num, size, c_amount;
-			int coins[] = {1, 2, 5, 10, 25};
+	int num, j, result;
+	int coins[] = {25, 10, 5, 2, 1};
 
-				if (argc != 2)
-						{
-									printf("Error\n");
-											return (1);
-												}
-					else
-							{
-										num = atoi(argv[1]);
-												size = 5;
-														if (num <= 0)
-																		printf("0\n");
-																else
-																			{
-																							c_amount = _change(num, &coins[0], size);
-																										printf("%d\n", c_amount);
-																												}
-																	}
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
 
-						return (0);
+	num = atoi(argv[1]);
+	result = 0;
+
+	if (num < 0)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	for (j = 0; j < 5 && num >= 0; j++)
+	{
+		while (num >= coins[j])
+		{
+			result++;
+			num -= coins[j];
+		}
+	}
+
+	printf("%d\n", result);
+	return (0);
 }
